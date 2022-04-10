5758dd96aa363f0a18fe4b413d122efcc220ca07
Dinos <papakosk@csd.auth.gr>
2022-04-10 14:32:09 +0200

    Implement baseline encoder (average embeddings)

---
+++ encoders/baseline.py
@@ -0,0 +1,10 @@
+from torch import Tensor
+from torch.nn import Module
+
+
+class BaselineEncoder(Module):
+    def forward(self, x: Tensor) -> Tensor:
+        # Count non-zero embeddings per batch
+        nonzero = (x.sum(dim=2) != 0.).sum(dim=1, keepdim=True)
+        # Average the individual word embeddings exlcuding pads
+        return x.sum(dim=1) / nonzero
