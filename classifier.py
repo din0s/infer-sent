98fbae2d65114f19448d8c2240f0b9dd29594b9b
Dinos <papakosk@csd.auth.gr>
2022-04-10 18:52:49 +0200

    Initial implementation of NLI classifier

---
+++ classifier.py
@@ -0,0 +1,100 @@
+from argparse import ArgumentParser
+from encoders import BaselineEncoder
+from pytorch_lightning import LightningModule
+from torch import Tensor
+from torch.nn import Embedding, Linear, Module, Sequential
+from torch.optim import Optimizer, SGD
+from torch.optim.lr_scheduler import _LRScheduler, ReduceLROnPlateau, StepLR
+from typing import Dict, List, Tuple
+
+import torch
+import torch.nn.functional as F
+import torchmetrics.functional as TF
+
+
+class Classifier(LightningModule):
+    @staticmethod
+    def add_model_specific_args(parent_parser: ArgumentParser):
+        parser = parent_parser.add_argument_group("Classifier")
+
+        parser.add_argument("--classifier_hidden_dim", type=int, default=512,
+            help="The dimensionality of the hidden layer in the MLP classifier.")
+
+        parser.add_argument("--lr", type=float, default=0.1,
+            help="The initial learning rate for the classifier.")
+
+        return parent_parser
+
+    def __init__(
+        self,
+        embeddings: Tensor,
+        encoder: Module,
+        n_classes: int,
+        classifier_hidden_dim: int,
+        lr: float,
+        **kwargs):
+        super().__init__()
+        self.save_hyperparameters("classifier_hidden_dim", "lr", ignore=["encoder"])
+
+        self.embed = Embedding.from_pretrained(embeddings, freeze=True)
+        self.encoder = encoder
+
+        if isinstance(encoder, BaselineEncoder):
+            rep_dim = 300
+
+        self.classifier = Sequential(
+            Linear(4 * rep_dim, self.hparams.classifier_hidden_dim),
+            Linear(self.hparams.classifier_hidden_dim, self.hparams.classifier_hidden_dim),
+            Linear(self.hparams.classifier_hidden_dim, n_classes)
+        )
+    
+    def forward(self, sentences: Tensor) -> Tensor:
+        u, v = torch.chunk(sentences, 2, dim=1)
+        u = self.encoder(self.embed(u))
+        v = self.encoder(self.embed(v))
+
+        z = torch.cat([u, v, torch.abs(u - v), u * v], dim=1)
+        z = self.classifier(z)
+        return z
+    
+    def step(self, batch: Dict[str, Tensor], stage: str) -> Tensor:
+        sentences, labels = batch['sentences'], batch['labels']
+        
+        logits = self(sentences)
+        loss = F.cross_entropy(logits, labels)
+        acc = TF.accuracy(logits, labels)
+
+        self.log(f'{stage}_acc', acc)
+        self.log(f'{stage}_loss', loss)
+        
+        return loss
+
+    def training_step(self, batch: Dict[str, Tensor], _: Tensor) -> Tensor:
+        return self.step(batch, 'train')
+
+    def validation_step(self, batch: Dict[str, Tensor], _: Tensor) -> Tensor:
+        return self.step(batch, 'val')
+
+    def test_step(self, batch: Dict[str, Tensor], _: Tensor) -> Tensor:
+        return self.step(batch, 'test')
+
+    def configure_optimizers(self) -> Tuple[List[Optimizer], List[_LRScheduler]]:
+        optimizer = SGD(self.parameters(), lr=self.hparams.lr)
+
+        scheduler1 = ReduceLROnPlateau(optimizer, mode="max", factor=0.2, patience=0)
+        scheduler2 = StepLR(optimizer, step_size=1, gamma=0.99)
+
+        scheduler1_cfg = {
+            "scheduler": scheduler1,
+            "interval": "epoch",
+            "monitor": "val_acc",
+            "name": "lr"
+        }
+
+        scheduler2_cfg = {
+            "scheduler": scheduler2,
+            "interval": "epoch",
+            "name": "lr"
+        }
+
+        return [optimizer], [scheduler1_cfg, scheduler2_cfg]
---
+++ train.py
@@ -0,0 +1,65 @@
+from argparse import ArgumentParser, Namespace
+from classifier import Classifier
+from encoders.baseline import BaselineEncoder
+from lr_stopping import LRStopping
+from pytorch_lightning import seed_everything, Trainer
+from pytorch_lightning.loggers import TensorBoardLogger
+from snli import SNLIDataModule
+
+import os
+
+def train(args: Namespace):
+    seed_everything(args.seed, workers=True)
+
+    snli = SNLIDataModule(args.data_dir, args.batch_size)
+    snli.prepare_data()
+    snli.setup(stage="fit")
+
+    if args.encoder_arch == "baseline":
+        encoder = BaselineEncoder()
+    else:
+        raise Exception(f"Unsupported encoder architecture '{args.encoder_arch}'")
+    
+    model = Classifier(snli.glove.vectors, encoder, n_classes=snli.num_classes, **vars(args))
+
+    log = TensorBoardLogger(args.log_dir, name=None, default_hp_metric=False)
+    gpus = 1 if args.use_gpu else 0
+    trainer = Trainer.from_argparse_args(args, logger=log, gpus=gpus, callbacks=[LRStopping()])
+    trainer.fit(model, snli)
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    
+    # Trainer arguments
+    parser.add_argument("--seed", type=int, default=420,
+        help="The seed to use for the RNG.")
+    
+    parser.add_argument("--max_epochs", type=int, default=-1,
+        help="The max amount of epochs to train the classifier.")
+    
+    parser.add_argument("--use_gpu", action='store_true', default=True,
+        help="Whether to use a GPU accelerator for training.")
+    
+    # Model arguments
+    Classifier.add_model_specific_args(parser)
+    
+    parser.add_argument("--encoder_arch", type=str, default="baseline",
+        choices=["baseline"],  # TODO: imeplement more
+        help="The name of the encoder architecture to use.")
+    
+    parser.add_argument("--batch_size", type=int, default=64,
+        help="The batch size used by the dataloaders.")
+    
+    # Directory arguments
+    parser.add_argument("--data_dir", type=str, default="./data",
+        help="The data directory to use for embeddings & datasets.")
+
+    parser.add_argument("--log_dir", type=str, default="./lightning_logs",
+        help="The logging directory for Pytorch Lightning.")
+
+    args = parser.parse_args()
+
+    if not os.path.exists(args.log_dir):
+        os.mkdir(args.log_dir)
+
+    train(args)
